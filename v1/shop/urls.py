50c958cb62b8a22c8714c7c9d6fc1a21289afd32
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-01-23 15:01:29 +0545

    BranchAdd view with authorization check

--- v1/shop/urls.py
+++ v1/shop/urls.py
@@ -1,4 +1,4 @@
-from .views import ShopCreateAPIView, ShopEditAPIView, ShopDeleteAPIView
+from .views import ShopCreateAPIView, ShopEditAPIView, ShopDeleteAPIView, BranchCreateAPIView
 from rest_framework.routers import SimpleRouter
 from django.urls import path
 
@@ -6,4 +6,5 @@ urlpatterns = [
   path('add/', ShopCreateAPIView.as_view(), name="create-shop"),
   path('<str:pk>/edit/', ShopEditAPIView.as_view(), name="edit-shop"),
   path('<str:pk>/delete/', ShopDeleteAPIView.as_view(), name="delete-shop"),
+  path('branch/add/', BranchCreateAPIView.as_view(), name="create-branch")
 ]
