ba4f475f3b80e9e7c850e5fd3ec8f37256960a4a
dhakalkushal <55182298+dhakalkushal@users.noreply.github.com>
2021-01-27 20:24:17 +0545

    modified filebase add settings in settings folder

--- config/asgi.py
+++ config/asgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.asgi import get_asgi_application
 
 #change the config.development to your required setting module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 
 application = get_asgi_application()
--- config/wsgi.py
+++ config/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 #change the config.development to your required setting module
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.development')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.development')
 
 application = get_wsgi_application()
